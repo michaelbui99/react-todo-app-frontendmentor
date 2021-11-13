6145abf1b8e01e54a1aafe81db1e9ba2790ee660
Michael Bui <michaelbui99@hotmail.com>
2021-11-14 00:20:01 +0100

    Refractor footer to separate component and added background image

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer class="attribution">
+      Challenge by{" "}
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by <a href="#">Michael Bui</a>.
+    </footer>
+  );
+};
+
+export default Footer;
