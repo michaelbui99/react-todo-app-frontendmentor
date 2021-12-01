c8e334fcab7a3ae89db5775f55a3762667f4f136
Michael Bui <michaelbui99@hotmail.com>
2021-12-01 22:56:58 +0100

    Implemented adding of new Todos on Enter keypress, added overflow-y scroll and custom scrollbar

--- src/components/todo-list/TodoList.jsx
+++ src/components/todo-list/TodoList.jsx
@@ -5,13 +5,15 @@ import TodoItem from "../todo-item/TodoItem";
 const TodoList = ({ todos, setTodos }) => {
   return (
     <div className="todo-list">
-      {todos.length > 0 ? (
-        todos.map((t) => (
-          <TodoItem todo={t} key={t.id} setTodos={setTodos} todos={todos} />
-        ))
-      ) : (
-        <p className="todo-item">No todos to show...</p>
-      )}
+      <div className="todo-list__todos">
+        {todos.length > 0 ? (
+          todos.map((t) => (
+            <TodoItem todo={t} key={t.id} setTodos={setTodos} todos={todos} />
+          ))
+        ) : (
+          <p className="todo-item">No todos to show...</p>
+        )}
+      </div>
 
       <TodoFooter todos={todos} />
     </div>
